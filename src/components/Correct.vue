432dea2a786958c3b01ae87ea64bc5d53cadb306
kkkkan <kan4649kan@gmail.com>
2021-07-25 11:56:30 +0900

    feat : LINEでシェアボタンとTwitterでシェアボタンも追加した

--- src/components/Correct.vue
+++ src/components/Correct.vue
@@ -4,29 +4,54 @@
       <p class="mb-6 indigo--text font-weight-black text-h4">正解！！</p>
       <p class="mb-6 indigo--text font-weight-medium">また遊んでね。</p>
       <v-container>
-        <v-row>
-          <v-col class="d-flex justify-end">
+        <v-row align="center">
+          <!--align="center"をつけると「もう一度遊ぶ」が縦方向中央に設置される-->
+          <v-col class="d-flex justify-end" cols="6">
             <v-btn
               class="mb-3 white--text"
-              color="green darken-1"
+              color="brown darken-4"
               v-on:click="retry"
             >
               もう一度遊ぶ
             </v-btn>
           </v-col>
-          <v-col class="d-flex justify-start">
-            <!-- style="text-transform: none" -> アルファベットを大文字にしない  -->
-            <v-btn
-              class="mb-3 white--text pb-3 pt-3"
-              color="blue darken-1"
-              v-on:click="shareByTwitter"
-              style="text-transform: none"
-            >
-              twitterで結果をシェア
-            </v-btn>
+          <v-col class="justify-start" cols="6">
+            <v-row>
+              <!-- style="text-transform: none" -> アルファベットを大文字にしない  -->
+              <v-btn
+                class="mb-3 white--text"
+                color="blue darken-1"
+                v-on:click="shareByTwitter"
+                style="text-transform: none"
+              >
+                twitterで結果をシェア
+              </v-btn>
+            </v-row>
+            <v-row>
+              <!-- style="text-transform: none" -> アルファベットを大文字にしない  -->
+              <v-btn
+                class="mb-3 white--text"
+                color="deep-purple darken-4"
+                v-on:click="shareByFacebook"
+                style="text-transform: none"
+              >
+                Facebookで結果をシェア
+              </v-btn>
+            </v-row>
+            <v-row>
+              <!-- style="text-transform: none" -> アルファベットを大文字にしない  -->
+              <v-btn
+                class="mb-3 white--text"
+                color="green accent-4"
+                v-on:click="shareByLine"
+                style="text-transform: none"
+              >
+                LINEで結果をシェア
+              </v-btn>
+            </v-row>
           </v-col>
-        </v-row></v-container
-      >
+        </v-row>
+      </v-container>
     </div>
   </div>
 </template>
@@ -51,6 +76,26 @@ export default {
         "ラジオ体操を正しく並び替え" +
         "&url=" +
         url;
+
+      location.href = shareURL;
+    },
+
+    shareByFacebook: function () {
+      // 現在のurlをエンコード
+      var url = encodeURIComponent(location.href);
+      // Twitter用のurl作成
+
+      let shareURL = "https://www.facebook.com/sharer/sharer.php?u=" + url;
+
+      location.href = shareURL;
+    },
+
+    shareByLine: function () {
+      // 現在のurlをエンコード
+      var url = encodeURIComponent(location.href);
+      // Twitter用のurl作成
+
+      let shareURL = "https://social-plugins.line.me/lineit/share?url=" + url;
 
       location.href = shareURL;
     },
